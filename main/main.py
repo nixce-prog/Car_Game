79296c62e4b86aeeadf8b1f9643c3e6412096993
nixce-prog <mcajkovskij272@gmail.com>
2024-06-20 16:53:41 +0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,17 +1,20 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from Player import Player
-
+player = None
 
 def root1():
-    root.destroy()
-
     root1 = Tk()
     root1.title('Car Game')
     root1.geometry('800x450')
     root1.resizable(False, False)
 
-    bg_image = Image.open(r'C:\Users\User\Desktop\Новая папка (9)\bg (1).png')
+
+    def r_destr():
+        root1.destroy()
+        roots()
+
+    bg_image = Image.open(r'bg.png')
     bg_photo = ImageTk.PhotoImage(bg_image)
     bg_label = Label(root1, image=bg_photo)
     bg_label.place(x=0, y=0, relwidth=1, relheight=1)
@@ -20,13 +23,51 @@ def root1():
     playbtn.place(x=600, y=320)
     storebtn = Button(root1, text='Магазин', font=('italic', 15), width=13, height=2)
     storebtn.place(x=621, y=250)
-    statsbtn = Button(root1, text='Статистика', font=('italic', 15), width=12, height=2)
+    statsbtn = Button(root1, text='Статистика', font=('italic', 15),command=r_destr, width=12, height=2)
     statsbtn.place(x=25, y=366)
 
     root1.mainloop()
 
 
+def roots():
+    global player
+
+    root2 = Tk()
+    root2.title('Car Game')
+    root2.geometry('800x450')
+    root2.resizable(False, False)
+
+    def r1_dest():
+        root2.destroy()
+        root1()
+
+    bg_image = Image.open(r'bg.png')
+    bg_photo = ImageTk.PhotoImage(bg_image)
+    bg_label = Label(root2, image=bg_photo)
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    lb_money = Label(root2, text=str(player.info['money']),font=('italic', 15))
+    lb_money.place(x=690,y=30)
+    lb_money1 = Label(root2, text="Деньга",font=('italic', 15))
+    lb_money1.place(x=590,y=30)
+    lb_win = Label(root2, text=str(player.info['win']),font=('italic', 15))
+    lb_win.place(x=690,y=60)
+    lb_win1 = Label(root2, text="Победы",font=('italic', 15))
+    lb_win1.place(x=590,y=60)
+    lb_bad = Label(root2, text=str(player.info['bad']),font=('italic', 15))
+    lb_bad.place(x=690,y=90)
+    lb_bad1 = Label(root2, text="Лузы",font=('italic', 15))
+    lb_bad1.place(x=590,y=90)
+
+    bkbtn = Button(root2, text='Назад',font=('italic', 15), command=r1_dest)
+    bkbtn.place(x=30,y=290)
+
+
+
+    root2.mainloop()
+
 def translate():
+    global player
     log = login.get()
     pas = password.get()
     player = Player(log, pas)
@@ -38,7 +79,7 @@ def translate():
     elif player.accaunt == 2:
         reglb = Label(root, text='Аккаунт был создан!', font=('italic', 14), bg='black', fg='white')
         reglb.place(x=15, y=20)
-        nxbtn = Button(root, text='Продолжить', font=('Italic', 12), command=root1, bg='green',
+        nxbtn = Button(root, text='Продолжить', font=('Italic', 12), command=root_dest, bg='green',
                                    fg='black', activebackground='green', activeforeground='white')
         nxbtn.place(x=360, y=310)
 
@@ -49,6 +90,11 @@ if __name__ == "__main__":
     root.geometry('800x450')
     root.resizable(False, False)
     root['bg'] = 'black'
+
+
+    def root_dest():
+        root.destroy()
+        root1()
 
     image_path = r"login.png"
     tk_image = PhotoImage(file=image_path)
