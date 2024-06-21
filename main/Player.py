a93b08e27a8311a8a4ca2ce419eafac0b37923ce
nixce-prog <mcajkovskij272@gmail.com>
2024-06-21 21:06:54 +0300

    Update Player.py

--- main/Player.py
+++ main/Player.py
@@ -1,18 +1,19 @@
 import json
 import os.path
 
+
 class Player():
-    car_all = {"Dodge":100,
-               "Lamborgini":1000
+    car_all = {"Dodge": 0,
+               "Lamborgini": 1000
                }
 
     default = [("money", 0, int),
-                ("win", 0, int), 
-                ("password", "0000", str), 
-                ("bad", 0, int),
-                ("car",{x[0]:False for x in car_all.items()},dict)
-                ]
-    
+               ("win", 0, int),
+               ("password", "0000", str),
+               ("bad", 0, int),
+               ("car", {x[0]: False for x in car_all.items()}, dict)
+               ]
+
     info_d = {x[0]: x[2](x[1]) for x in default}
 
     def __init__(self, login, password):
@@ -55,11 +56,11 @@ class Player():
             with open("profile.json", "w+") as f:
                 info = Player.info_d
                 info["password"] = password
-                json.dump({login: info,"system":{login:len(str(info))}}, f)
+                json.dump({login: info, "system": {login: len(str(info))}}, f)
                 self.accaunt = 2
 
-        g = {x[0]:x[2] for x in Player.default}
-        self.info = {x[0]:g[x[0]](x[1]) for x in info.items()}
+        g = {x[0]: x[2] for x in Player.default}
+        self.info = {x[0]: g[x[0]](x[1]) for x in info.items()}
         self.login = login
 
     def save(self):
@@ -71,9 +72,7 @@ class Player():
             s["system"][self.login] = len(str(self.info))
             json.dump(s, f)
 
-
-
-    def buy(self,car):
+    def buy(self, car):
         if self.info["money"] >= Player.car_all[car]:
             self.info['money'] -= Player.car_all[car]
             self.info["car"][car] = True
@@ -81,7 +80,6 @@ class Player():
             return True
         else:
             return False
-
 
 
 
