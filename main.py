ace86f13360adfbbc10a63b0cdd54589dadf27aa
mexa9999 <Arhipovdana48@gmail.com>
2024-06-21 19:58:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,114 @@
-from tkinter import *
+def game():
+    pygame.init()
+        
+    window = pygame.display.set_mode((1200,700))
+    Clok = pygame.time.Clock()
 
-root = Tk()
-root.title('Car_Game')
-root.geometry('800x450')
-root['bg'] = 'black'
 
-def root1():
-    
+    image = pygame.image.load("dggame.png")
+    image = pygame.transform.scale(image,(150,50))
+    image2 = pygame.image.load("road.png")
+    image2 = pygame.transform.scale(image2,(1200,700))
 
-image_path = r"C:\Users\mcajk\OneDrive\Документы\Py\car\login.gif"
-tk_image = PhotoImage(file=image_path)
 
-image_label = Label(root, image=tk_image)
-image_label.pack()
+    class sprite(object):
 
-login = Entry(root,
-              width=30
-              )
-login.place(x=250,y=220)
+        sprite_list = {}
 
-password = Entry(root,
-              width=30
-              )
-password.place(x=250,y=260)
+        def __init__(self,speed,size,image,x,y,max_speed,hard) -> None:
+            self.speed = speed
+            self.size = size
+            self.image = pygame.transform.scale(image,size)
+            self.image_old = image
+            self.x = x
+            self.y = y
+            self.max_speed = max_speed
+            self.max_speed_old = max_speed
+            self.hard = hard
+            
+            sprite.sprite_list[self] = [self.image,[x,y]]
+        def obnow():
+            for x in sprite.sprite_list:
+                x.image = pygame.transform.scale(x.image_old,x.size)
+                sprite.sprite_list[x] = [x.image,[x.x,x.y]]
+        def exit():
+            sprite.sprite_list={}
 
-accept = Button(root,
-                text='Принять',
-                font=('Italic', 12),
-                command= ,
-                bg='grey',
-                fg='black',
-                activebackground='green',
-                activeforeground='white'
-                )
-accept.place(x=360, y=310)
 
-lb_login = Label(root, text='Логин:', font=('Italic', 12), bg='black', fg='white')
-lb_login.place(x=190,y=215)
-lb_password = Label(root, text='Пароль:', font=('Italic', 12), bg='black', fg='white')
-lb_password.place(x=180,y=257)
 
-root.mainloop()
+    class player(sprite):
+        pass
+        
+
+        
+    interesting = 50
+
+
+    s = player(0,[100,100],image,100,100,300,1)
+    g = sprite(1,(100,100),image,100,200,200,random.randrange(int(interesting/2),interesting)/100)
+    g = sprite(1,(100,100),image,100,300,30,random.randrange(int(interesting/2),interesting)/100)
+    g = sprite(1,(100,100),image,100,400,30,random.randrange(int(interesting/2),interesting)/100)
+
+    g_r = random.randrange(200)/100
+
+
+    x_a = 0
+
+
+    runing = True
+
+    race_time = (60)/2
+    Fps = 60
+    t = 0
+
+
+    while runing:
+        t += 1
+        keys = pygame.key.get_pressed()
+
+        x_a += s.speed
+        if x_a >= 1200:
+            x_a = 0
+
+        for x in sprite.sprite_list:
+            if x != s:
+                x.x += x.speed - s.speed
+                if x.speed < x.max_speed/2:
+                    x.speed += x.max_speed/race_time/Fps*x.hard
+
+            else:
+                if not keys[pygame.K_d]:
+                    if x.speed > 0:
+                        x.speed *= 0.995
+                else:
+                    if s.speed < s.max_speed/2:
+                        s.speed += s.max_speed/race_time/Fps
+
+
+        sprite.obnow()
+            
+
+
+
+
+        window.blit(image2,(0-x_a,0))
+        window.blit(image2,(1200-x_a,0))
+
+
+        window.blits([*sprite.sprite_list.values()])
+
+
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runing = False
+                pygame.quit()
+        Clok.tick(Fps)
+        if t//60 >= race_time:
+            p = [x[1] for x in sorted({x.x:x for x in sprite.sprite_list}.items())]
+            f = p.index(s)
+            runing = False
+            pygame.quit()
+            return f*interesting
